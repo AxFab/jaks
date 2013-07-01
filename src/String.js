87815c1b783c2544a894948170b4cd33f1eed0e4
Fabien Bavent <fabien.bavent@gmail.com>
2013-07-01 19:36:55 +0200

    [+] CORE :: Add String & Date

---
+++ src/String.js
@@ -0,0 +1,27 @@
+/*
+
+    Jaks - Graphic Framework for JavaScript
+
+    Copyright (C) 2013  Fabien Bavent<fabien.bavent@gmail.com>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Jaks.  If not, see <http://www.gnu.org/licenses/>.
+*/
+String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g, '');};
+String.prototype.ltrim=function(){return this.replace(/^\s+/,'');};
+String.prototype.rtrim=function(){return this.replace(/\s+$/,'');};
+String.prototype.fulltrim=function(){return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' ');};
+String.prototype.startwith=function(str){ return this.substring(0, str.length) == str;};
+String.prototype.endswith=function(str){ return this.substring(this.length - str.length, this.length) == str;};
+
+
